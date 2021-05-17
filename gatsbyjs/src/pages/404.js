761d999183908c0d7c41d9c373bcf2af4b134fe8
Lisi Linhart <ewl@storyblok.com>
2021-05-17 12:19:05 +0200

    feat: add 404 page

---
+++ gatsbyjs/src/pages/404.js
@@ -0,0 +1,27 @@
+import * as React from "react"
+import SbEditable from 'storyblok-react'
+
+import Layout from "../components/layout"
+import DynamicComponent from "../components/DynamicComponent"
+import useStoryblok from "../utils/storyblok"
+
+const NotFoundPage = ({ location }) => { 
+  let components = null
+  let story = useStoryblok(null, location)
+
+  if(story) {
+    components = story.content.body.map(blok => {
+      return (<DynamicComponent blok={blok} key={blok._uid} />)
+    })
+  }
+
+  return (
+  <Layout>
+    <SbEditable content={story ? story.content : false }>
+    <h1>{ story ? story.name : 'Not Found' }</h1>
+    { components }
+    </SbEditable>
+  </Layout>
+)}
+
+export default NotFoundPage
