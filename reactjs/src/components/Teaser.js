0afca48a1b86a2035ee3e48225fda7cca0e772ee
Chakit Arora <chakit@192.168.1.17>
2022-11-04 17:55:40 +0530

    remove react folder

--- reactjs/src/components/Teaser.js
+++
@@ -1,8 +0,0 @@
-
-import { storyblokEditable } from "@storyblok/react";
-
-const Teaser = ({ blok }) => {
-  return <h2 style={{textAlign: 'center'}} {...storyblokEditable(blok)} >{blok.headline}</h2>;
-};
- 
-export default Teaser;
